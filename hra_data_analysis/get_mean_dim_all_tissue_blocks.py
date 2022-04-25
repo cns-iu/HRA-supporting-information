9f47556a419e4a7924af929f46b3eab4aa36f263
andreasbueckle <abueckle@indiana.edu>
2022-04-25 10:50:48 -0400

    Update get_mean_dim_all_tissue_blocks.py

--- hra_data_analysis/get_mean_dim_all_tissue_blocks.py
+++ hra_data_analysis/get_mean_dim_all_tissue_blocks.py
@@ -6,7 +6,7 @@ def main():
     # To get TOKEN for viewing unpublished data, go to EUI, log in, then view source, then copy token from browser
     # TOKEN = sys.argv[1] if len(sys.argv) > 1 else None
     TOKEN = ''
-    HBM_LINK = 'https://hubmap-link-api.herokuapp.com/hubmap-datasets?format=jsonld'
+    HBM_LINK = 'https://ccf-api.hubmapconsortium.org/v1/hubmap/rui_locations.jsonld'
     if TOKEN:
         HBM_LINK += '&token=' + TOKEN
 
@@ -17,21 +17,19 @@ def main():
         x = []
         y = []
         z = []
-
+        counter = 0
+        
         for item in data['@graph']:
             for sample in item['samples']:
+                counter = counter + 1
                 x.append(sample['rui_location']['x_dimension'])
                 y.append(sample['rui_location']['y_dimension'])
                 z.append(sample['rui_location']['z_dimension'])
-
+                
+    print("Number of tissue blocks: " + str(counter))
     print(compute_average(x))
     print(compute_average(y))
     print(compute_average(z))
-
-# 15.602722772277227
-# 13.616336633663366
-# 9.009158415959613
-
 
 def compute_average(list):
     """takes a list of numbers and returns the average
