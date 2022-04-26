51103a4679dec284fce6f4f9186cf258025d94c2
andreasbueckle <abueckle@indiana.edu>
2022-04-26 11:28:43 -0400

    Update get_mean_dim_all_tissue_blocks.py

--- hra_data_analysis/get_mean_dim_all_tissue_blocks.py
+++ hra_data_analysis/get_mean_dim_all_tissue_blocks.py
@@ -1,35 +1,32 @@
-import json
-import urllib.request
+import requests
 
 
 def main():
-    # To get TOKEN for viewing unpublished data, go to EUI, log in, then view source, then copy token from browser
-    # TOKEN = sys.argv[1] if len(sys.argv) > 1 else None
-    TOKEN = ''
-    HBM_LINK = 'https://ccf-api.hubmapconsortium.org/v1/hubmap/rui_locations.jsonld'
-    if TOKEN:
-        HBM_LINK += '&token=' + TOKEN
 
     # first, let's get the most recent data
-    with urllib.request.urlopen(HBM_LINK) as url:
-        data = json.loads(url.read().decode())
+    # To get TOKEN for viewing unpublished data, go to EUI, log in, then view source, then copy token from browser
+    TOKEN = ""
+    endpoint = "https://ccf-api.hubmapconsortium.org/v1/hubmap/rui_locations.jsonld"
 
-        x = []
-        y = []
-        z = []
-        counter = 0
-        
-        for item in data['@graph']:
-            for sample in item['samples']:
-                counter = counter + 1
-                x.append(sample['rui_location']['x_dimension'])
-                y.append(sample['rui_location']['y_dimension'])
-                z.append(sample['rui_location']['z_dimension'])
-                
+    data = requests.get(endpoint, params={"token": TOKEN}).json()
+
+    x = []
+    y = []
+    z = []
+    counter = 0
+
+    for item in data['@graph']:
+        for sample in item['samples']:
+            counter = counter + 1
+            x.append(sample['rui_location']['x_dimension'])
+            y.append(sample['rui_location']['y_dimension'])
+            z.append(sample['rui_location']['z_dimension'])
+
     print("Number of tissue blocks: " + str(counter))
     print(compute_average(x))
     print(compute_average(y))
     print(compute_average(z))
+
 
 def compute_average(list):
     """takes a list of numbers and returns the average
